c6e78976a573e7d8d6b337b81208390eaaaf7704
cluck-mur <cluck.mur@outlook.jp>
2021-06-24 14:10:23 +0900

    Seedも使ってデータを登録してみた(2) ・./seeders/xxxxxxxxxxxxxx-test-user.jsを修正した

--- hello_world_mvc/seeders/20210624045803-test-user.js
+++ hello_world_mvc/seeders/20210624045803-test-user.js
@@ -11,6 +11,14 @@ module.exports = {
      *   isBetaMember: false
      * }], {});
     */
+    const now = new Date();
+    return queryInterface.bulkInsert('Users', [
+      { name: 'テストA', email: 'aaa@example.com', password: 'aaa-password', createdAt: now, updatedAt: now },
+      { name: 'テストB', email: 'bbb@example.com', password: 'bbb-password', createdAt: now, updatedAt: now },
+      { name: 'テストC', email: 'ccc@example.com', password: 'ccc-password', createdAt: now, updatedAt: now },
+      { name: 'テストD', email: 'ddd@example.com', password: 'ddd-password', createdAt: now, updatedAt: now },
+      { name: 'テストE', email: 'eee@example.com', password: 'eee-password', createdAt: now, updatedAt: now },
+    ], {});
   },
 
   down: async (queryInterface, Sequelize) => {
@@ -20,5 +28,6 @@ module.exports = {
      * Example:
      * await queryInterface.bulkDelete('People', null, {});
      */
+    return queryInterface.bulkDelete('Users', null, {});
   }
 };
