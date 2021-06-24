0ad6a2db5dfb44bbf94f975559aa28750afd98f3
cluck-mur <cluck.mur@outlook.jp>
2021-06-24 15:00:24 +0900

    "https://blog.otasys.co.jp/2019/07/02/express-2/" の 「2.4 静的ページのルーティング」項までやった

--- hello_world_mvc/routes/users.js
+++ hello_world_mvc/routes/users.js
@@ -2,8 +2,26 @@ var express = require('express');
 var router = express.Router();
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
+router.get('/', function (req, res, next) {
   res.send('respond with a resource');
+});
+
+/* ユーザーIDを指定できるルート */
+router.get('/:userId', function (req, res, next) {
+  var userId = req.params.userId;
+  console.log('ユーザーID ' + userId + ' が指定されました。');
+  res.send('ok');
+});
+
+/* ユーザーのリストをJSON出力する */
+router.get('/json/', function (req, res, next) {
+  // Sequelizeのモデルを使ってデータを取得する
+  dbModels.User.findAll().then(users => {
+
+    if (!users) return next(); // 404 Error
+
+    res.json(users);
+  });
 });
 
 module.exports = router;
